75a923a18e352fbf26ebe9f722c99bd7b227eb61
Klymenko Illia <sovuh2703@gmail.com>
2023-05-03 15:18:57 +0300

    Created logout logic

--- django_project/accounts/urls.py
+++ django_project/accounts/urls.py
@@ -7,6 +7,7 @@ from .forms import CustomAuthenticationForm
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html' ,authentication_form=CustomAuthenticationForm), name='login')
+    path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html' ,authentication_form=CustomAuthenticationForm), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 from .forms import CustomUserCreationForm
 
 # Create your views here.
-def register(request):
+def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -13,3 +16,8 @@ def register(request):
     return render(request, 'accounts/register.html', {
         'form': form,
     })
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('core:index')
